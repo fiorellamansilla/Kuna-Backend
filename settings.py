c8be9e0e89c4f482d254b7419a632d95be9aba7e
Diego Mansilla <dmansilla07@gmail.com>
2022-10-09 22:32:49 -0300

    Remove unnecessary imports

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
-from crypt import methods
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from marshmallow import fields
+from flask import Flask
 
 # Create an instance of the Flask App 
 app = Flask(__name__)
@@ -10,4 +6,6 @@ app = Flask(__name__)
 # Configure our Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/kuna_db'
 app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
+if __name__ == '__main__':
+    app.run()
