efc7e3390cc91f4a4e6500f0865bc8f4c77e82c2
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2022-10-13 12:44:17 +0200

    Item Model modificated - images column added in object

--- models/item.py
+++ models/item.py
@@ -14,13 +14,14 @@ class Item(db.Model):
     discount = db.Column (db.Float, nullable=False, server_default="0")
     SKU = db.Column (db.String(128), nullable=False)
     quantity_stock = db.Column (db.Integer, nullable=False, server_default="0")
+    images = db.Column(db.String(256), nullable=False)
 
     def create(self):
         db.session.add(self)
         db.session.commit()
         return self
 
-    def __init__(self,name_item,desc_item,size,price,discount,SKU,quantity_stock):
+    def __init__(self,name_item,desc_item,size,price,discount,SKU,quantity_stock,images):
         self.name_item = name_item
         self.desc_item = desc_item
         self.size = size
@@ -28,6 +29,7 @@ class Item(db.Model):
         self.discount = discount
         self.SKU = SKU
         self.quantity_stock = quantity_stock
+        self.images = images
 
     def __repr__(self):
         return '' % self.item_id
@@ -47,4 +49,5 @@ class ItemSchema(SQLAlchemyAutoSchema):
     discount = fields.Number(required=True)
     SKU = fields.String(required=True)
     quantity_stock = fields.Number(required=True)
+    images = fields.String(required=True)
     
