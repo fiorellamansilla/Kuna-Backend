c95b3822b01285ae646f97dec714ee842c119edd
Diego Mansilla <dmansilla07@gmail.com>
2022-10-24 22:09:06 -0300

    Add user entity

--- entities/user.py
+++ entities/user.py
@@ -1,10 +1,41 @@
+from datetime import datetime
+import hashlib
+
+CONST_SALT = "5gz"
+
 class User:
-    def __init__(self, id, username, password):
-        self.id = id
+    # Constructor
+    def __init__(self, username=None, password="", email=None, country=None):
+        p = password + CONST_SALT
         self.username = username
-        self.password = password
+        self.__password_hash = hashlib.md5(p.encode()).hexdigest()
+        self.email = email
+        self.country = country
+        self.is_blocked = False
+        self.is_approved = True
+        self.creation_date = datetime.today().isoformat()
+        self.purchased_items = 0
 
     def __repr__(self):
-        pass
+        return str(self.__dict__)
 
+    def change_password(self, new_password):
+        p = new_password + CONST_SALT
+        self.__password_hash = hashlib.md5(p.encode().hexdigest())
 
+    def change_country(self, new_country):
+        self.country = new_country
+
+    def change_email(self, new_email):
+        self.email = new_email
+
+    def buy_something(self):
+        self.purchased_items = self.purchased_items + 1
+
+    def get_password(self):
+        return self.__password_hash
+
+    def is_correct_password(self, password_attempt):
+        p = password_attempt + CONST_SALT
+        hashed_attempt = hashlib.md5(p.encode())
+        return hashed_attempt.hexdigest() ==  self.__password_hash
