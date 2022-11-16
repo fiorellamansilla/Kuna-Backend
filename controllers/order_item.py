0086e0da6bf25160d420bbd0226ad13b36479e76
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2022-11-16 19:01:44 +0100

    Update Method implemented for Association object

--- controllers/order_item.py
+++ controllers/order_item.py
@@ -29,3 +29,23 @@ def get_by_id(order_item_id):
     order_item_entity = OrderItemEntity(order_item)
     return make_response(jsonify({"order_item": order_item_entity.toJSON()}))
 
+# PUT Method / UPDATE
+def update_by_id(order_item_id):
+    data = request.get_json()
+    get_order_item = OrderItem.query.get(order_item_id)
+
+    if data.get('order_id'):
+        get_order_item.order_id = data['order_id']
+    if data.get('item_id'):
+        get_order_item.item_id = data['item_id']
+    if data.get('quantity_ordered'):
+        get_order_item.quantity_ordered = data['quantity_ordered']
+    if data.get('total_amount'):
+        get_order_item.total_amount = data['total_amount']
+
+
+    db.session.add(get_order_item)
+    db.session.commit()
+    order_item_schema = OrderItemSchema(only=['order_id','item_id','quantity_ordered','total_amount'])
+    order_item = order_item_schema.dump(get_order_item)
+    return make_response(jsonify({"order_item": order_item}))
